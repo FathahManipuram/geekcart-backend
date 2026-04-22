b03f14f2060cb8cdfd6fde21f305375554c3320e
Fathah Manipuram <abdulfathahmtmpm@gmail.com>
2026-04-22 18:30:46 +0530

    Initial commit: backend structure and dependencies

---
+++ src/server.js
@@ -0,0 +1,12 @@
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+
+
+const app= express()
+
+
+const PORT= process.env.PORT
+app.listen(PORT, ()=>{
+	console.log(`Server is running on port ${PORT}`)
+})
