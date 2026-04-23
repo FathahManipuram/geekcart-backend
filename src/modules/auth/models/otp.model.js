380f4cfb830347e4a7f0365580c0d3fbdee0b1b1
Fathah Manipuram <abdulfathahmtmpm@gmail.com>
2026-04-23 23:49:50 +0530

    feat(auth): add OTP model with expiry and attempt tracking

---
+++ src/modules/auth/models/otp.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose"
+import User from "../../user/models/user.model.js"
+
+const otpSchema= new mongoose.Schema({
+	userId:{type: mongoose.Schema.Types.ObjectId, ref:"USer"},
+	email: {type: String},
+	otp:{type: String, required:true},
+	type:{type: String, 
+		enum:["email-verify", "password-reset"],
+		required: true,
+	},
+
+	attemptCount: {type: Number, default:0},
+	maxAttempt: {type: Number, default:5},
+
+	expiresAt: {type: Date, require:true},
+},
+{Timestamp: true}
+);
+
+
+otpSchema.index({expiresAt: 1}, {expireAfterSeconds:0})
+export default mongoose.model("otp", otpSchema)
