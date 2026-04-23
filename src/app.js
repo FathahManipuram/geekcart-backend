81c8fb8bffe045bfc48813972923703943c501ab
Fathah Manipuram <abdulfathahmtmpm@gmail.com>
2026-04-23 23:47:22 +0530

    feat(core): setup express app and route structure

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express from "express"
+import cors from "cors"
+import morgan from "morgan"
+import routes from "../src/routes/index.js"
+import { errorMiddleware } from "./common/middleware/error.middleware.js"
+
+const app= express()
+
+app.use(cors())
+app.use(express.json())
+app.use(morgan("dev"))
+
+app.use("/api/v1",routes)
+
+
+app.use(errorMiddleware)
+
+
+
+export default app
