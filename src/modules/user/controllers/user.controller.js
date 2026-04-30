cc8429324a2fff03810a4a026413bfa8ecaaae26
Fathah Manipuram <abdulfathahmtmpm@gmail.com>
2026-04-30 14:31:57 +0530

    feat(user): add user profile controller

---
+++ src/modules/user/controllers/user.controller.js
@@ -0,0 +1,86 @@
+import { HTTP_STATUS } from "../../../common/constants/statusCode.js"
+import { successResponse } from "../../../common/helpers/response.js"
+import * as userService from "../services/user.service.js"
+
+//Get profile
+export const getProfileController= async(req, res, next)=>{
+	try{
+		const userId= req.user.id
+		const result= await userService.getProfileService(userId)
+		return successResponse(res, 
+		HTTP_STATUS.OK,
+		result.message,
+		result.data
+		)
+	}catch(err){
+		next(err)
+	}
+}
+
+
+//Update profile
+export const updateProfileController= async (req, res, next)=>{
+	try{
+		const userId= req.user.id
+		const result= await userService.updateProfileService(userId, req.body)
+		return successResponse(
+			res,
+			HTTP_STATUS.OK,
+			result.message,
+			result.data
+		)
+	}catch(err){
+		next(err)
+	}
+}
+
+
+//Change email
+export const changeEmailController= async(req, res, next)=>{
+	try{
+		const userId= req.user.id
+		const {email}= req.body
+		const result= await userService.changeEmailService(userId, email)
+		return successResponse(
+			res,
+			HTTP_STATUS.OK,
+			result.message
+		)
+	}catch(err){
+		next(err)
+	}
+}
+
+
+//Verify email change
+export const verifyEmailChangeController= async(req, res, next)=>{
+	try{
+		const userId= req.user.id
+		const {email, otp}= req.body
+
+		const result= await userService.verifyEmailChangeService(userId, email, otp)
+		return successResponse(
+			res,
+			HTTP_STATUS.OK,
+			result.message
+		)
+	}catch(err){
+		next(err)
+	}
+}
+
+
+//Change password
+export const changePasswordController= async(req, res, next)=>{
+	try{
+		const userId= req.user.id
+		const result= await userService.changePasswordService(userId, req.body)
+		return successResponse(
+			res,
+			HTTP_STATUS.OK,
+			result.message
+		)
+	}catch(err){
+		next(err)
+	}
+}
