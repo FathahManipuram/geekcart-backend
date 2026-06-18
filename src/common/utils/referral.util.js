0174cb76a8cd16f6904e158241b818e85216276f
Fathah Manipuram <abdulfathahmtmpm@gmail.com>
2026-06-18 16:20:13 +0530

    feat: add referral code generation utility function

---
+++ src/common/utils/referral.util.js
@@ -0,0 +1,35 @@
+import { User } from "../../modules/user-side/user-profile/models/user.model.js";
+
+
+
+export const generateReferralCode = async (fullName) => {
+  let referralCode;
+  let exists = true;
+
+
+  const cleanName = fullName && fullName.trim().length > 0 ? fullName : "USER";
+
+  const prefix = "GC";
+  const namePart = cleanName
+    .replace(/\s+/g, "")
+    .toUpperCase()
+    .slice(0, 3)
+    .padEnd(3, "X");
+
+  while (exists) {
+    const numberPart = Math.floor(1000 + Math.random() * 9000);
+
+    const suffix = Math.random()
+      .toString(36)
+      .substring(2, 5)
+      .toUpperCase()
+      .padEnd(3, "Z"); 
+
+    referralCode = `${prefix}${namePart}${numberPart}${suffix}`;
+
+
+    exists = await User.exists({ referralCode });
+  }
+
+  return referralCode;
+};
